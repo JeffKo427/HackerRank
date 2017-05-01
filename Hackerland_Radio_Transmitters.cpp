78f81e0c18ce394dc6df6944a700e4e94ce7d60f
Jeff <JeffKo427@gmail.com>
2017-05-01 15:11:06 -0500

    YEEEEAAAH

--- Hackerland_Radio_Transmitters.cpp
+++ Hackerland_Radio_Transmitters.cpp
@@ -27,35 +27,36 @@ int main() {
     int n;
     int k;
     cin >> n >> k;
-    vector<int> x(n);
+    vector<int> houses(n);
     for (int x_i = 0;x_i < n;x_i++) {
-        cin >> x[x_i];
+        cin >> houses[x_i];
     }
     vector<int> transmitters;
-    sort(x.begin(), x.end());
+    sort(houses.begin(), houses.end());
     
-    int idealPlacement = x.front() + k;
-    int previousHouse, nextHouse;
-    for (int house : x) {
-        //cout << house << " ";
-        nextHouse = 0;
-        for (int i = 0; i < n; i++) {
-            if (x.at(i) > house + k) {
-                nextHouse = x.at(i);
-                break;
+    // Delete all duplicates from x
+    houses.erase( unique( houses.begin(), houses.end() ), houses.end() );
+    //cout << houses.size() << endl;
+    
+    int distanceToLastTransmitter, prev;
+    for (int i = 0; i < houses.size(); i++) {
+        if (transmitters.size())
+            distanceToLastTransmitter = houses.at(i) - transmitters.back();
+        else
+            distanceToLastTransmitter = k + i + 1;
+        
+        if (distanceToLastTransmitter > k) {
+            prev = i;
+            for (int j = i+1; j < houses.size(); j++) {
+                if (houses.at(j) - houses.at(i) > k) {
+                    break;
+                }
+                prev = j;
             }
+            transmitters.push_back(houses.at(prev));
         }
-        if (house == idealPlacement) {
-            transmitters.push_back(house);
-            idealPlacement = max(nextHouse + k, house + 1 + 2*k);
-        } else if (house > idealPlacement) {
-            transmitters.push_back(previousHouse);
-            idealPlacement = max(nextHouse + k, house + 1 + 2*k);
-        }
-        previousHouse = house;
     }
-    if (previousHouse > (transmitters.back() + k))
-        transmitters.push_back(previousHouse);
+    
     /*
     cout << endl;
     for (int t : transmitters) {
