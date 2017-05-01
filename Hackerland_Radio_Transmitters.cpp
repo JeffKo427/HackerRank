8a1edd0782ce48d649a37bf24884e843d5389e62
Jeff Alan <JeffKo427@gmail.com>
2017-05-01 13:58:17 -0500

    Still Broken

--- Hackerland_Radio_Transmitters.cpp
+++ Hackerland_Radio_Transmitters.cpp
@@ -37,34 +37,33 @@ int main() {
     int idealPlacement = x.front() + k;
     int previousHouse, nextHouse;
     for (int house : x) {
-        cout << house << " ";
+        //cout << house << " ";
+        nextHouse = 0;
         for (int i = 0; i < n; i++) {
-            if (x.at(i) > house) {
+            if (x.at(i) > house + k) {
                 nextHouse = x.at(i);
                 break;
             }
         }
         if (house == idealPlacement) {
             transmitters.push_back(house);
-            idealPlacement = nextHouse + k;
+            idealPlacement = max(nextHouse + k, house + 1 + 2*k);
         } else if (house > idealPlacement) {
             transmitters.push_back(previousHouse);
-            idealPlacement = nextHouse + k;
+            idealPlacement = max(nextHouse + k, house + 1 + 2*k);
         }
         previousHouse = house;
     }
     if (previousHouse > (transmitters.back() + k))
         transmitters.push_back(previousHouse);
-    
+    /*
     cout << endl;
     for (int t : transmitters) {
         cout << t << " ";
     }
     cout << endl;
-    
-    
+    */
     cout << transmitters.size();
     
     return 0;
 }
-
