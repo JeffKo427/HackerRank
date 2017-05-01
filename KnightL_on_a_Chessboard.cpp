01cac4aebff7dd4829361643d816dacd7494f71b
Jeff <JeffKo427@gmail.com>
2017-04-30 20:21:20 -0500

    implemented broken method

--- KnightL_on_a_Chessboard.cpp
+++ KnightL_on_a_Chessboard.cpp
@@ -26,7 +26,7 @@ using namespace std;
 struct Node {
   public: 
     int m,n;
-    Square(int,int);
+    Node(int,int);
     Node *parent;
     vector<Node *> children;
 };
@@ -44,18 +44,65 @@ void clearChessboard(bool **chessboard, int n) {
     }
 }
 
+Node *graphMoves(Node *root, int a, int b, bool **chessboard, int n) {
+    // add all non-repeat moves to tree
+    bool movesAdded = false;
+    // always eight possible moves: +a+b, +a-b, -a+b, -a-b where a in x b in y and same where a in y b in x
+    int moves[4][2] = {{1,1},{1,-1},{-1,1},{-1,-1}};
+    int newPos[2];
+    for (int *move : moves) {
+        newPos[0] = root->m + a * move[0];
+        newPos[1] = root->n + b * move[1];
+        if (!chessboard[newPos[0]][newPos[1]]) {
+            movesAdded = true;
+            Node *temp = new Node(newPos[0], newPos[1]);
+            temp->parent = root;
+            if (newPos[0] == n && newPos[1] == n) {
+                return temp;
+            }
+        }
+        newPos[0] = root->m + b * move[0];
+        newPos[1] = root->n + a * move[1];
+        if (!chessboard[newPos[0]][newPos[1]]) {
+            movesAdded = true;
+            Node *temp = new Node(newPos[0], newPos[1]);
+            temp->parent = root;
+            if (newPos[0] == n && newPos[1] == n) {
+                return temp;
+            }
+        }
+    }
+    
+    if (movesAdded)
+        graphMoves();
+}
+
+int getDepth(Node *leaf, Node *root, int depth) {
+    if (leaf == root) {
+        return depth;
+    } else {
+        depth = getDepth(leaf->parent, root, depth+1);
+    }
+    
+    return -1;
+}
+
 int main(){
     int n;
     cin >> n;
     // your code goes here
-    bool chessboard[][] = new bool[n][n];
+    bool **chessboard = new bool*[n][n];
+    Node *solution;
     for (int a = 1; a < n; a++) {
         for (int b = a; b < n; b++) {
             clearChessboard(chessboard, n);
-            Node root = Node(0,0);
+            Node *root = new Node(0,0);
             chessboard[0][0] = true;
-            
+            solution = graphMoves(root, a, b, chessboard, n);
+            cout << getDepth(solution, root, 0) << " ";
         }
+        cout << endl;
     }
+    
     return 0;
 }
